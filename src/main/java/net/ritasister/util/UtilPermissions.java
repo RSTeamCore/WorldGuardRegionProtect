37f113eea448a8eb9a165e0b37a03e9aede31525
Rita Wolf <ritasistr@mail.ru>
2021-10-07 14:11:53 +0300

    renamed perm variables

--- src/main/java/net/ritasister/util/UtilPermissions.java
+++ src/main/java/net/ritasister/util/UtilPermissions.java
@@ -5,35 +5,13 @@ import org.bukkit.permissions.*;
 public interface UtilPermissions 
 {
 	public static final String 
-	/* Main Permission */
-	all_perm_plugin = ("srp.*"),
-	all_perm = ("*"),
-	/* Command Permission */
-	reload_cfg = ("srp.reload.cfg"),
-	reload_msg_cfg = ("srp.reload.cfg.msg"),
-	/* Listener Permission */
-	regionProtect = ("srp.regionprotect"),
-	regionProtectAllow = ("srp.regionprotectallow");
-}
-/*public enum UtilPermissions 
-{
-	all_perm_plugin("aqualimecommand.*"),
-	all_perm("*"),
-	
-	reload_cfg("srp.reload.cfg"),
-	reload_msg_cfg("srp.reload.cfg.msg"),
-	
-	serverRegionProtect("srp.serverregionprotect"),
-	serverRegionProtectAllow("srp.serverregionallow");
-	
-	private String perm;
+	all_perm_plugin=("srp.*"),
+	all_perm=("*"),
 
-    private UtilPermissions(String perm) 
-    {
-        this.perm=perm;
-    }
-    public String getPerm() 
-    {
-        return perm;
-    }
-}*/
+	reload_cfg=("wgrp.reload.cfg"),
+	reload_msg_cfg=("wgrp.reload.cfg.msg"),
+
+	regionProtectInfoAdmin=("wgrgprotect.info.admin"),
+	regionProtect=("srp.regionprotect"),
+	regionProtectAllow=("srp.regionprotectallow");
+}
