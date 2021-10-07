5e8307b87597cff5390763c5e12bdd6def4f0518
Rita Wolf <ritasistr@mail.ru>
2021-10-07 14:40:19 +0300

    change perms from 'srp' to 'wgrp'

--- src/main/java/net/ritasister/util/UtilPermissions.java
+++ src/main/java/net/ritasister/util/UtilPermissions.java
@@ -5,13 +5,13 @@ import org.bukkit.permissions.*;
 public interface UtilPermissions 
 {
 	public static final String 
-	all_perm_plugin=("srp.*"),
+	all_perm_plugin=("wgrp.*"),
 	all_perm=("*"),
 
 	reload_cfg=("wgrp.reload.cfg"),
 	reload_msg_cfg=("wgrp.reload.cfg.msg"),
 
 	regionProtectInfoAdmin=("wgrgprotect.info.admin"),
-	regionProtect=("srp.regionprotect"),
-	regionProtectAllow=("srp.regionprotectallow");
+	regionProtect=("wgrp.regionprotect"),
+	regionProtectAllow=("wgrp.regionprotectallow");
 }
