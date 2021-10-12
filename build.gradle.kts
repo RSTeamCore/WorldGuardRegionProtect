909a249f0733ee55583ed46c2f788aa8477ae07c
Rita Wolf <ritasistr@mail.ru>
2021-10-12 22:23:53 +0300

    updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
     mavenCentral()
+    maven {url = uri("https://maven.enginehub.org/repo/") }
     maven {url = uri("https://papermc.io/repo/repository/maven-public/")}
 }
 
@@ -17,9 +18,9 @@ dependencies {
     compileOnly ("com.destroystokyo.paper:paper-api:1.16.5-R0.1-SNAPSHOT")
     compileOnly ("com.sk89q.worldguard:worldguard-bukkit:7.0.4")
 
-    implementation(kotlin("stdlib"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.31")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
 
 tasks.getByName<Test>("test") {
