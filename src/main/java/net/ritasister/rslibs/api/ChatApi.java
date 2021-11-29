76e68c1ef0754d8cab207d6a8e390567d124ff2c
Rita Wolf <ritasistr@mail.ru>
2021-11-29 16:42:40 +0200

    renamed from Color.java

---
+++ src/main/java/net/ritasister/rslibs/api/ChatApi.java
@@ -0,0 +1,31 @@
+package net.ritasister.rslibs.api;
+
+import org.bukkit.ChatColor;
+
+public class ChatApi {
+
+    /**
+     * Get colour for any message.
+     *
+     * @return message
+     */
+    public static String getColorCode(String message) {
+        return ChatColor.translateAlternateColorCodes('&', message);
+    }
+    /**
+     * Get \\n space from config for sender message without color.
+     *
+     * @return message
+     */
+    public static String getDoubleTabSpaceWithoutColor(String message) {
+        return message.replace("\\n", "\n");
+    }
+    /**
+     * Get \\n space from config for sender message with color.
+     *
+     * @return message
+     */
+    public static String getDoubleTabSpaceWithColor(String message) {
+        return getColorCode(message.replace("\\n", "\n"));
+    }
+}
