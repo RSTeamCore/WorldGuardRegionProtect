cd40ab95535246ae03230c804bda1e491ed400b6
Rita Wolf <ritasistr@mail.ru>
2021-12-09 14:04:38 +0200

    up to 1.18 version api

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,21 +8,23 @@ allprojects {
     apply(plugin = "maven-publish")
 
     java {
-        toolchain {languageVersion.set(JavaLanguageVersion.of(11))}
+        toolchain {languageVersion.set(JavaLanguageVersion.of(17))}
     }
 }
 
 group = "net.ritasister.srp"
-version = "0.7.1-pre2"
+version = "0.7.1-pre1"
 description = "WorldGuardRegionProtect"
 
 repositories {
     mavenCentral()
+    //WorldGuard
     maven {url = uri("https://maven.enginehub.org/repo/")}
+    //PaperMC
     maven {url = uri("https://papermc.io/repo/repository/maven-public/")}
 }
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.18-R0.1-SNAPSHOT")
-    compileOnly ("com.sk89q.worldguard:worldguard-bukkit:7.0.4")
+    compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.4")
 }
