3df9948c1020f6598f172f05278ac74af8f7ab65
Rita Wolf <ritasistr@mail.ru>
2021-12-10 12:23:10 +0200

    return java 16 for support 1.17 to 1.18

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,12 +8,12 @@ allprojects {
     apply(plugin = "maven-publish")
 
     java {
-        toolchain {languageVersion.set(JavaLanguageVersion.of(17))}
+        toolchain {languageVersion.set(JavaLanguageVersion.of(16))}
     }
 }
 
 group = "net.ritasister.srp"
-version = "0.7.1-pre1"
+version = "0.7.1-pre2"
 description = "WorldGuardRegionProtect"
 
 repositories {
@@ -25,6 +25,11 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.18-R0.1-SNAPSHOT")
     compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.4")
+    //Paper 1.17.1
+    compileOnly("io.papermc.paper:paper-api:1.17.1-R0.1-SNAPSHOT")
+    //try to support old verions Paper 1.12.2
+    //compileOnly("com.destroystokyo.paper:paper-api:1.12.2-R0.1-SNAPSHOT")
+    //compileOnly("com.sk89q.worldguard:worldguard-legacy:6.2")
+    //compileOnly("com.sk89q.worldedit:worldedit-bukkit:6.1.4-SNAPSHOT")
 }
