61b9ac87cba7744f7e9c1ccfc74cae5146504487
Rita Wolf <ritasistr@mail.ru>
2022-01-05 18:32:12 +0200

    refactor

--- src/main/java/net/ritasister/rslibs/utils/UpdateChecker.java
+++ src/main/java/net/ritasister/rslibs/utils/UpdateChecker.java
@@ -9,13 +9,11 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.Consumer;
 
-public class UpdateChecker 
-{
+public class UpdateChecker {
 	private JavaPlugin plugin;
     private int resourceId;
     
-    public UpdateChecker(JavaPlugin plugin, int resourceId) 
-    {
+    public UpdateChecker(JavaPlugin plugin, int resourceId) {
         this.plugin = plugin;
         this.resourceId = resourceId;
     }
@@ -24,15 +22,11 @@ public class UpdateChecker
 	 * 
 	 * @param consumer
 	 */
-    public void getVersion(final Consumer<String> consumer) 
-    {
-        Bukkit.getScheduler().runTaskAsynchronously(this.plugin, () -> 
-        {
+    public void getVersion(final Consumer<String> consumer) {
+        Bukkit.getScheduler().runTaskAsynchronously(this.plugin, () -> {
             try (InputStream inputStream = new URL("https://api.spigotmc.org/legacy/update.php?resource=" + this.resourceId)
-            		.openStream(); Scanner scanner = new Scanner(inputStream)) 
-            {
-                if(scanner.hasNext())
-                {
+            		.openStream(); Scanner scanner = new Scanner(inputStream)) {
+                if(scanner.hasNext()) {
                     consumer.accept(scanner.next());
                 }
             }catch(IOException exception){
