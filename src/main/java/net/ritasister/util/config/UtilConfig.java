26aceb36abcb20ac4746604c31fbad3e23ef2b17
Rita Wolf <ritasistr@mail.ru>
2022-02-10 18:09:48 +0200

    migrate patch's from utilConfig and UtilConfigMessage

--- src/main/java/net/ritasister/util/config/UtilConfig.java
+++ src/main/java/net/ritasister/util/config/UtilConfig.java
@@ -1,9 +1,7 @@
 package net.ritasister.util.config;
 
 import java.util.*;
-
-import org.bukkit.configuration.ConfigurationSection;
-
+import net.ritasister.rslibs.api.RSApi;
 import net.ritasister.wgrp.WorldGuardRegionProtect;
 
 public final class UtilConfig {
@@ -32,27 +30,26 @@ public final class UtilConfig {
 		WorldGuardRegionProtect.instance.saveDefaultConfig();
 		WorldGuardRegionProtect.instance.reloadConfig();
 
-		this.regionProtect = (List<String>) getPatch().getList("region_protect", new ArrayList<String>());
-		this.regionProtectAllow = (List<String>) getPatch().getList("region_protect_allow", new ArrayList<String>());
-		this.regionProtectOnlyBreakAllow = (List<String>) getPatch().getList(".region_protect_only_break_allow", new ArrayList<String>());
-		this.spawnEntityType = (List<String>) getPatch().getList("spawn_entity_type", new ArrayList<String>());
-		this.interactType = (List<String>) getPatch().getList("interact_type", new ArrayList<String>());
+		this.regionProtect = (List<String>) RSApi.getPatch().getList("region_protect", new ArrayList<String>());
+		this.regionProtectAllow = (List<String>) RSApi.getPatch().getList("region_protect_allow", new ArrayList<String>());
+		this.regionProtectOnlyBreakAllow = (List<String>) RSApi.getPatch().getList(".region_protect_only_break_allow", new ArrayList<String>());
+		this.spawnEntityType = (List<String>) RSApi.getPatch().getList("spawn_entity_type", new ArrayList<String>());
+		this.interactType = (List<String>) RSApi.getPatch().getList("interact_type", new ArrayList<String>());
 
-		this.cmdWe = (List<String>) getPatch().getList("no_protect_cmd.command_we", new ArrayList<String>());
-		this.cmdWeC = (List<String>) getPatch().getList("no_protect_cmd.command_c", new ArrayList<String>());
-		this.cmdWeP = (List<String>) getPatch().getList("no_protect_cmd.command_p", new ArrayList<String>());
-		this.cmdWeS = (List<String>) getPatch().getList("no_protect_cmd.command_s", new ArrayList<String>());
-		this.cmdWeU = (List<String>) getPatch().getList("no_protect_cmd.command_u", new ArrayList<String>());
-		this.cmdWeCP = (List<String>) getPatch().getList("no_protect_cmd.command_cp", new ArrayList<String>());
-		this.cmdWeB = (List<String>) getPatch().getList("no_protect_cmd.command_b", new ArrayList<String>());
-		this.spyCommand = (List<String>) getPatch().getList("spy_command.command_list", new ArrayList<String>());
+		this.cmdWe = (List<String>) RSApi.getPatch().getList("no_protect_cmd.command_we", new ArrayList<String>());
+		this.cmdWeC = (List<String>) RSApi.getPatch().getList("no_protect_cmd.command_c", new ArrayList<String>());
+		this.cmdWeP = (List<String>) RSApi.getPatch().getList("no_protect_cmd.command_p", new ArrayList<String>());
+		this.cmdWeS = (List<String>) RSApi.getPatch().getList("no_protect_cmd.command_s", new ArrayList<String>());
+		this.cmdWeU = (List<String>) RSApi.getPatch().getList("no_protect_cmd.command_u", new ArrayList<String>());
+		this.cmdWeCP = (List<String>) RSApi.getPatch().getList("no_protect_cmd.command_cp", new ArrayList<String>());
+		this.cmdWeB = (List<String>) RSApi.getPatch().getList("no_protect_cmd.command_b", new ArrayList<String>());
+		this.spyCommand = (List<String>) RSApi.getPatch().getList("spy_command.command_list", new ArrayList<String>());
 
-		this.regionMessageProtect = getPatch().getBoolean("protect_message");
-		this.regionMessageProtectWe = getPatch().getBoolean("protect_we_message");
-		this.spyCommandNotifyConsole = getPatch().getBoolean("spy_command.notify.console");
-		this.spyCommandNotifyAdmin = getPatch().getBoolean("spy_command.notify.admin");
-		this.spyCommandNotifyAdminPlaySoundEnable = getPatch().getBoolean("spy_command.notify.sound");
-		this.spyCommandNotifyAdminPlaySound = getPatch().getString("spy_command.notify.sound_type");
+		this.regionMessageProtect = RSApi.getPatch().getBoolean("protect_message");
+		this.regionMessageProtectWe = RSApi.getPatch().getBoolean("protect_we_message");
+		this.spyCommandNotifyConsole = RSApi.getPatch().getBoolean("spy_command.notify.console");
+		this.spyCommandNotifyAdmin = RSApi.getPatch().getBoolean("spy_command.notify.admin");
+		this.spyCommandNotifyAdminPlaySoundEnable = RSApi.getPatch().getBoolean("spy_command.notify.sound");
+		this.spyCommandNotifyAdminPlaySound = RSApi.getPatch().getString("spy_command.notify.sound_type");
 	}
-	private ConfigurationSection getPatch() {return WorldGuardRegionProtect.instance.getConfig().getConfigurationSection("worldguard_protect_region.");}
 }
--- src/main/java/net/ritasister/util/config/UtilConfigMessage.java
+++ src/main/java/net/ritasister/util/config/UtilConfigMessage.java
@@ -1,11 +1,16 @@
 package net.ritasister.util.config;
 
 import net.ritasister.rslibs.api.ChatApi;
+import net.ritasister.rslibs.api.RSApi;
 import org.bukkit.configuration.ConfigurationSection;
 
 import net.ritasister.util.UtilLoadConfig;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 
 public class UtilConfigMessage {
+
 	public String wgrpMsg;
 	public String wgrpMsgWe;
 	public String noPerm;
@@ -18,21 +23,40 @@ public class UtilConfigMessage {
 	public String configMsgReloaded;
 	public String configMsgNotFound;
 
+	public String dbConnectSuccessfull;
+	public String dbConnectError;
+	public String dbConnectFailed;
+	public String dbReconnect;
+	public String dbClosePSTError;
+	public String dbCloseRSError;
+	public String dbCloseDBError;
+	public String dbLoadError;
+	public String dbLoadAsyncError;
+
 	public UtilConfigMessage() {
 		try{
-			wgrpMsg = ChatApi.getColorCode(getMessageConfig().getString("messages.ServerMsg.wgrpMsg", wgrpMsg));
-			wgrpMsgWe = ChatApi.getColorCode(getMessageConfig().getString("messages.ServerMsg.wgrpMsg_We", wgrpMsgWe));
-			noPerm = ChatApi.getColorCode(getMessageConfig().getString("messages.ServerMsg.noPerm", noPerm));
-			sendAminInfo = ChatApi.getColorCode(getMessageConfig().getString("messages.ServerMsg.sendAminInfo", sendAminInfo));
-			wgrpUseHelp = ChatApi.getColorCode(getMessageConfig().getString("messages.ExampleUseCommand.wgrpUseHelp", wgrpUseHelp));
+			wgrpMsg = ChatApi.getColorCode(RSApi.getStringMessageColor("messages.ServerMsg.wgrpMsg", wgrpMsg));
+			wgrpMsgWe = ChatApi.getColorCode(RSApi.getStringMessageColor("messages.ServerMsg.wgrpMsg_We", wgrpMsgWe));
+			noPerm = ChatApi.getColorCode(RSApi.getStringMessageColor("messages.ServerMsg.noPerm", noPerm));
+			sendAminInfo = ChatApi.getColorCode(RSApi.getStringMessageColor("messages.ServerMsg.sendAminInfo", sendAminInfo));
+			wgrpUseHelp = ChatApi.getColorCode(RSApi.getStringMessageColor("messages.ExampleUseCommand.wgrpUseHelp", wgrpUseHelp));
 
-			configReloaded = ChatApi.getColorCode(getMessageConfig().getString("messages.Configs.configReloaded", configReloaded));
-			configNotFound = ChatApi.getColorCode(getMessageConfig().getString("messages.Configs.configNotFound", configNotFound));
-			configMsgReloaded = ChatApi.getColorCode(getMessageConfig().getString("messages.Configs.configMsgReloaded", configMsgReloaded));
-			configMsgNotFound = ChatApi.getColorCode(getMessageConfig().getString("messages.Configs.configMsgNotFound", configMsgNotFound));
+			configReloaded = ChatApi.getColorCode(RSApi.getStringMessageColor("messages.Configs.configReloaded", configReloaded));
+			configNotFound = ChatApi.getColorCode(RSApi.getStringMessageColor("messages.Configs.configNotFound", configNotFound));
+			configMsgReloaded = ChatApi.getColorCode(RSApi.getStringMessageColor("messages.Configs.configMsgReloaded", configMsgReloaded));
+			configMsgNotFound = ChatApi.getColorCode(RSApi.getStringMessageColor("messages.Configs.configMsgNotFound", configMsgNotFound));
+
+			dbConnectSuccessfull=ChatApi.getColorCode(RSApi.getStringMessageColor("messages.database.dbConnectSuccessfull", dbConnectSuccessfull));
+			dbConnectError=ChatApi.getColorCode(RSApi.getStringMessageColor("messages.database.dbConnectError", dbConnectError));
+			dbConnectFailed=ChatApi.getColorCode(RSApi.getStringMessageColor("messages.database.dbConnectFailed", dbConnectFailed));
+			dbReconnect=ChatApi.getColorCode(RSApi.getStringMessageColor("messages.database.dbReconnect", dbReconnect));
+			dbClosePSTError=ChatApi.getColorCode(RSApi.getStringMessageColor("messages.database.dbClosePSTError", dbClosePSTError));
+			dbCloseRSError=ChatApi.getColorCode(RSApi.getStringMessageColor("messages.database.dbCloseRSError", dbCloseRSError));
+			dbCloseDBError=ChatApi.getColorCode(RSApi.getStringMessageColor("messages.database.dbCloseDBError", dbCloseDBError));
+			dbLoadError=ChatApi.getColorCode(RSApi.getStringMessageColor("messages.database.dbLoadError", dbLoadError));
+			dbLoadAsyncError=ChatApi.getColorCode(RSApi.getStringMessageColor("messages.database.dbLoadAsyncError", dbLoadAsyncError));
 		}catch(NullPointerException ex){
 			ex.printStackTrace();
 		}
 	}
-	private ConfigurationSection getMessageConfig() {return UtilLoadConfig.messages;}
 }
