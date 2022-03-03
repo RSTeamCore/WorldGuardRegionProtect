bd886f824428551d9055e52d862b0d588fdfa9e4
Rita Wolf <ritasistr@mail.ru>
2022-03-04 00:42:36 +0200

    add perm for spy for suspect and for admin who can spy for suspect

--- src/main/java/net/ritasister/util/IUtilPermissions.java
+++ src/main/java/net/ritasister/util/IUtilPermissions.java
@@ -4,5 +4,6 @@ public interface IUtilPermissions {
 	String commandWGRP=("wgrp.command.wgrpbase");
 	String regionProtectNotifyAdmin=("wgrp.notify.admin");
 	String regionProtect=("wgrp.regionprotect");
-	String spySuspectInteractRegion=("wgrp.spy.suspectinteract");
+	String spyInspectForSuspect=("wgrp.spy.spyinspectforsuspect");
+	String spyAdminForSuspect=("wgrp.spy.spyadminforsuspect");
 }
