7d7542023914aa9be94aa9403e5ed33526b90573
Rita Wolf <ritasistr@mail.ru>
2022-03-04 20:32:46 +0200

    -

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ logger.lifecycle("""
  If you encounter trouble:
  1) Try running 'build' in a separate Gradle run
  2) Use gradlew and not gradle
- 3) If you have a problem, you can join us discord 
+ 3) If you have a problem, you can join us discord https://discord.gg/kvqvA3GTVF
  Output files will be in [subproject]/build/libs
 *******************************************
 """)
