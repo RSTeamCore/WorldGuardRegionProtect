ab6a0b9555b4dbfc1831e3e4a6894de0f70b9bbd
RitaWolf <ritasistr@mail.ru>
2022-05-04 00:02:29 +0300

    removed old versions support

--- wgrp-bukkit/src/main/java/net/ritasister/register/RegisterListener.java
+++ wgrp-bukkit/src/main/java/net/ritasister/register/RegisterListener.java
@@ -10,13 +10,8 @@ public class RegisterListener {
 	
 	public static void RegisterEvents(PluginManager pm) {
 		try{
-			if(RSApi.isVersionV1_16() && RSApi.isVersionV1_17()) {
-				final RegionProtect regionProtect = new RegionProtect(WorldGuardRegionProtect.getInstance());
-				pm.registerEvents(regionProtect, WorldGuardRegionProtect.getInstance());
-				RSLogger.info("&6You are using server version 1.16. Dont are used any features from 1.17+");
-			}else{
-				RSLogger.info("&6You are using server version 1.17 or higher. Used all last feauters of these versions.");
-			}
+			final RegionProtect regionProtect = new RegionProtect(WorldGuardRegionProtect.getInstance());
+			pm.registerEvents(regionProtect, WorldGuardRegionProtect.getInstance());
 			RSLogger.info("&2All listeners load successfully!");
 		}catch(Exception e){
             RSLogger.err("Could load all listeners... We have a error!");
