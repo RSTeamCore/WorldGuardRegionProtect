9a7e234f87ad8689cc311e217694151530d50f42
RitaWolf <ritasistr@mail.ru>
2022-05-15 15:56:51 +0300

    migrated methods for storage into RSStorage

---
+++ wgrp-bukkit/src/main/java/net/ritasister/rslibs/api/RSStorage.java
@@ -0,0 +1,34 @@
+package net.ritasister.rslibs.api;
+
+import net.ritasister.rslibs.datasource.StorageDataBase;
+import net.ritasister.rslibs.datasource.StorageDataSource;
+import net.ritasister.wgrp.WorldGuardRegionProtect;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class RSStorage {
+
+    public StorageDataSource dbLogsSource;
+    public ConcurrentHashMap<UUID, StorageDataBase> dbLogs = new ConcurrentHashMap<>();
+
+    /**
+     * @param uniqueId get player uuid from storage.
+     *
+     * @return getDataStorage.
+     */
+    @NotNull
+    public StorageDataBase getDataStorage(UUID uniqueId) {
+        return this.dbLogs.get(uniqueId);
+    }
+
+    /**
+     *
+     * @return getDataSource.
+     */
+    @NotNull
+    public StorageDataSource getDataSource() {
+        return this.dbLogsSource;
+    }
+}
