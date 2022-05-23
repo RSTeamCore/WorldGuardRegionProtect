0d8e007ceb8eafd57cd1fee13025053275a42a9b
RitaWolf <ritasistr@mail.ru>
2022-05-23 15:12:36 +0300

    removed unused

--- wgrp-api/src/main/java/net/ritasister/wgrp/rslibs/api/interfaces/IListenerHandler.java
+++
@@ -1,8 +0,0 @@
-package net.ritasister.wgrp.rslibs.api.interfaces;
-
-import org.bukkit.plugin.PluginManager;
-
-public interface IListenerHandler {
-
-    void listenerHandler(PluginManager pluginManager);
-}
