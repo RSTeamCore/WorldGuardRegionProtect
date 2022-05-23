af08e049df2b430a62232c5d508d5321e2cdd443
RitaWolf <ritasistr@mail.ru>
2022-05-23 15:12:36 +0300

    removed unused

--- wgrp-api/src/main/java/net/ritasister/wgrp/rslibs/api/interfaces/IChatApi.java
+++
@@ -1,8 +0,0 @@
-package net.ritasister.wgrp.rslibs.api.interfaces;
-
-public interface IChatApi {
-
-    String getColorCode(String message);
-    String getDoubleTabSpaceWithoutColor(String message);
-    String getDoubleTabSpaceWithColor(String message);
-}
--- wgrp-api/src/main/java/net/ritasister/wgrp/rslibs/api/interfaces/ICommandHandler.java
+++
@@ -1,6 +0,0 @@
-package net.ritasister.wgrp.rslibs.api.interfaces;
-
-public interface ICommandHandler {
-
-    void commandHandler();
-}
