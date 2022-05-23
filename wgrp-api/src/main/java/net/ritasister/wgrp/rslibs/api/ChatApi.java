bc092e159bcf2721d95b60c36d931c48f6dffaa6
RitaWolf <ritasistr@mail.ru>
2022-05-23 22:44:56 +0300

    removed interfaces

--- wgrp-api/src/main/java/net/ritasister/wgrp/rslibs/api/ChatApi.java
+++ wgrp-api/src/main/java/net/ritasister/wgrp/rslibs/api/ChatApi.java
@@ -1,10 +1,9 @@
 package net.ritasister.wgrp.rslibs.api;
 
-import net.ritasister.wgrp.rslibs.api.interfaces.IChatApi;
 import org.bukkit.ChatColor;
 import org.jetbrains.annotations.NotNull;
 
-public class ChatApi implements IChatApi {
+public class ChatApi {
 
     /**
      * Get colour for any message.
@@ -13,23 +12,5 @@ public class ChatApi implements IChatApi {
      */
     public String getColorCode(@NotNull String message) {
         return ChatColor.translateAlternateColorCodes('&', message);
-    }
-
-    /**
-     * Get \\n space from config for sender message without color.
-     *
-     * @return message
-     */
-    public String getDoubleTabSpaceWithoutColor(@NotNull String message) {
-        return message.replace("\\n", "\n");
-    }
-
-    /**
-     * Get \\n space from config for sender message with color.
-     *
-     * @return message
-     */
-    public String getDoubleTabSpaceWithColor(@NotNull String message) {
-        return getColorCode(message.replace("\\n", "\n"));
     }
 }
