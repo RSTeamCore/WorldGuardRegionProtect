f53a76f9afc8e2e8867b1847c795f3f6e20df9e0
RitaWolf <ritasistr@mail.ru>
2022-05-23 22:41:09 +0300

    added subcommand to reload config.yml

--- wgrp-bukkit/src/main/java/net/ritasister/wgrp/command/CommandWGRP.java
+++ wgrp-bukkit/src/main/java/net/ritasister/wgrp/command/CommandWGRP.java
@@ -17,7 +17,7 @@ import java.util.*;
 public final class CommandWGRP extends CmdExecutor {
 
 	private final WorldGuardRegionProtect wgRegionProtect;
-	private final List<String> subCommand = List.of("help", "about", "spy");
+	private final List<String> subCommand = List.of("reload", "help", "about", "spy");
 
 	public CommandWGRP(WorldGuardRegionProtect wgRegionProtect) {
 		super(UtilCommandList.WGRP.toString());
@@ -29,7 +29,7 @@ public final class CommandWGRP extends CmdExecutor {
 		final boolean sp = sender instanceof Player;
 		FileConfiguration configPatch = wgRegionProtect.getUtilLoadConfig().config;
 		FileConfiguration getConfig = wgRegionProtect.getWgrpBukkitPlugin().getConfig();
-		if(sp && wgRegionProtect.getRsApi().isSenderCommandsPermission((Player) sender, cmd, IUtilPermissions.COMMAND_WGRP,
+		if(sp && wgRegionProtect.getRsApi().isSenderCommandsPermission(sender, cmd, IUtilPermissions.COMMAND_WGRP,
                 wgRegionProtect.utilConfigMessage.noPerm))return; {}
 		if(args.length > 0) {
 			runCommand(sender, cmd, args, configPatch, getConfig);
@@ -38,12 +38,30 @@ public final class CommandWGRP extends CmdExecutor {
 		}
 	}
 
-	private void runCommand(CommandSender sender, Command cmd, String @NotNull [] args, FileConfiguration configPatch, FileConfiguration getConfig) {
+	private void runCommand(@NotNull CommandSender sender, Command cmd, String @NotNull [] args, FileConfiguration configPatch, FileConfiguration getConfig) {
 		final boolean sp = sender instanceof Player;
-		if (args[0].equalsIgnoreCase("help")) {
-			sender.sendMessage(wgRegionProtect.getChatApi().getDoubleTabSpaceWithColor("sub-commands:\n/wgrp about\n/wgrp spy - enable or disable spying for log who interact with region.\n/wgrp add protect|protect-allow|only-break-allow <region name>"));
-		} else if (args[0].equalsIgnoreCase("about")) {
-			sender.sendMessage(wgRegionProtect.getChatApi().getDoubleTabSpaceWithColor("&b===================================================\n&aHi! If you need help from this plugin, you can contact with me on: \n&ehttps://www.spigotmc.org/resources/wgRegionProtect-1-12.81333/\n&ehttp://rubukkit.org/threads/sec-wgRegionProtect-0-7-0-pre2-dop-zaschita-dlja-regionov-wg-1-13-1-17.171324/page-4#post-1678435\n&b===================================================\n&aBut if you find any error or you want to send me any idea for this plugin&b, \n&aso you can create issues on github:&e https://github.com/RitaSister/WorldGuardRegionProtect/issues\n&6your welcome!"));
+		if(args[0].equalsIgnoreCase("reload")) {
+			wgRegionProtect.getUtilLoadConfig().getConfig().reload();
+			sender.sendMessage(wgRegionProtect.getChatApi().getColorCode(wgRegionProtect.getUtilConfigMessage().configReloaded));
+		}
+		if(args[0].equalsIgnoreCase("help")) {
+			sender.sendMessage(wgRegionProtect.getChatApi().getColorCode("""
+									sub-commands:
+									/wgrp about
+									/wgrp spy - enable or disable spying for log who interact with region.
+									/wgrp add protect|protect-allow|only-break-allow <region name>
+									"""));
+		} else if(args[0].equalsIgnoreCase("about")) {
+			sender.sendMessage(wgRegionProtect.getChatApi().getColorCode("""
+									&b=======================================================================
+									&aHi! If you need help from this plugin, you can contact with me on:
+									&ehttps://www.spigotmc.org/resources/wgRegionProtect-1-12.81333/
+									&ehttp://rubukkit.org/threads/sec-wgRegionProtect-0-7-0-pre2-dop-zaschita-dlja-regionov-wg-1-13-1-17.171324/page-4#post-1678435
+									&b=======================================================================
+									&aBut if you find any error or you want to send me any idea for this plugin&b,
+									&aso you can create issues on github:&e https://github.com/RSTeamCore/WorldGuardRegionProtect
+									&6your welcome!
+									"""));
 		} else if(sp && wgRegionProtect.getRsApi().isSenderCommandsPermission((Player) sender, cmd, IUtilPermissions.SPY_INSPECT_ADMIN_COMMAND,
                 wgRegionProtect.utilConfigMessage.noPerm))return; {
 			if(args[0].equalsIgnoreCase("spy")) {
@@ -57,25 +75,6 @@ public final class CommandWGRP extends CmdExecutor {
 				}
 			}
 		}
-		/*if (args.length == 2 && args[0].equalsIgnoreCase("add") && sender.hasPermission("opsrg.admin")) {
-			wgRegionProtect.functions.add(List.of(args[1]), sender, false);
-		}
-		if (args.length == 2 && args[0].equalsIgnoreCase("del") && sender.hasPermission("opsrg.admin")) {
-			wgRegionProtect.functions.rem(List.of(args[1]), sender, false);
-		}
-		if (args.length == 2 && args[0].equalsIgnoreCase("addmine") && sender.hasPermission("opsrg.admin")) {
-			wgRegionProtect.functions.add(List.of(args[1]), sender, true);
-		}
-		if (args.length == 2 && args[0].equalsIgnoreCase("delmine") && sender.hasPermission("opsrg.admin")) {
-			wgRegionProtect.functions.rem(List.of(args[1]), sender, true);
-		}
-		if (args.length <= 2 && args[0].equalsIgnoreCase("list") && sender.hasPermission("opsrg.admin")) {
-			wgRegionProtect.getRsRegion().paginate(sender, args, false);
-			return;
-		}
-		if (args.length <= 2 && args[0].equalsIgnoreCase("listmine") && sender.hasPermission("opsrg.admin")) {
-			wgRegionProtect.getRsRegion().paginate(sender, args, true);
-		}*/
 	}
 
 	@Override
