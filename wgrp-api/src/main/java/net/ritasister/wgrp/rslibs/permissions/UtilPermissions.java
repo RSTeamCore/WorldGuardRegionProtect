6df37b5c9295c67ac26b5690fec62385739c1087
RitaWolf <ritasistr@mail.ru>
2022-05-26 20:51:15 +0300

    added star permission

--- wgrp-api/src/main/java/net/ritasister/wgrp/rslibs/permissions/UtilPermissions.java
+++ wgrp-api/src/main/java/net/ritasister/wgrp/rslibs/permissions/UtilPermissions.java
@@ -3,9 +3,13 @@ package net.ritasister.wgrp.rslibs.permissions;
 public enum UtilPermissions {
 
 	NULL_PERM(""),
+	PERMISSION_STAR("*"),
+	//Command permission
 	COMMAND_WGRP("wgrp.command.wgrpbase"),
 	COMMAND_WGRP_RELOAD_CONFIGS("wgrp.command.reload"),
 	COMMAND_SPY_INSPECT_ADMIN("wgrp.command.spy"),
+
+	//Listeners permission
 	REGION_PROTECT("wgrp.regionprotect"),
 	REGION_PROTECT_NOTIFY_ADMIN("wgrp.notify.admin"),
 	SPY_INSPECT_FOR_SUSPECT("wgrp.spy.suspect"),
