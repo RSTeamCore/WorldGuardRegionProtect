894a062c8cc0c86e6abcfa152cfcf5afac965af2
iRedTea <red__tea@outlook.com>
2022-05-27 18:12:52 +0300

    Added help, addregion, removeregion commands. Added worlds in config

--- wgrp-api/src/main/java/net/ritasister/wgrp/rslibs/permissions/UtilPermissions.java
+++ wgrp-api/src/main/java/net/ritasister/wgrp/rslibs/permissions/UtilPermissions.java
@@ -11,6 +11,8 @@ public enum UtilPermissions {
 	COMMAND_ADDREGION("wgrp.command.addregion"),
 	COMMAND_REMOVEREGION("wgrp.command.removeregion"),
 
+	COMMAND_ADDREGION("wgrp.addregion"),
+
 	//Listeners permission
 	REGION_PROTECT("wgrp.regionprotect"),
 	REGION_PROTECT_NOTIFY_ADMIN("wgrp.notify.admin"),
