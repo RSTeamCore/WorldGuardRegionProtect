a326698fcf6fbb9d732113a3c286aecc9ebb013a
RitaWolf <ritasistr@mail.ru>
2022-06-05 17:01:27 +0300

    fixed NPE before try to get name of region

--- wgrp-bukkit/src/main/java/net/ritasister/wgrp/rslibs/api/RSRegion.java
+++ wgrp-bukkit/src/main/java/net/ritasister/wgrp/rslibs/api/RSRegion.java
@@ -1,11 +1,22 @@
 package net.ritasister.wgrp.rslibs.api;
 
+import com.sk89q.worldedit.IncompleteRegionException;
+import com.sk89q.worldedit.LocalSession;
+import com.sk89q.worldedit.WorldEdit;
 import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldedit.math.BlockVector3;
+import com.sk89q.worldedit.regions.CuboidRegion;
+import com.sk89q.worldedit.regions.Region;
 import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.protection.ApplicableRegionSet;
+import com.sk89q.worldguard.protection.managers.RegionManager;
+import com.sk89q.worldguard.protection.regions.ProtectedCuboidRegion;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import com.sk89q.worldguard.protection.regions.RegionContainer;
 import net.ritasister.wgrp.rslibs.api.interfaces.IRSRegion;
+import net.ritasister.wgrp.rslibs.exceptions.NoSelectionException;
 import org.bukkit.Location;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
@@ -54,11 +65,54 @@ public class RSRegion implements IRSRegion {
     /**
      * Get region name.
      *
-     * @param location location of object.
+     * @param player location of object.
+     */
+    @Override
+    public String getProtectRegionName(final Player player) {
+        LocalSession localSession = WorldEdit.getInstance().getSessionManager().get(BukkitAdapter.adapt(player));
+        Region selection = null;
+        try {
+            selection = localSession.getSelection(BukkitAdapter.adapt(player.getWorld()));
+        } catch (IncompleteRegionException ignored) {}
+        try {
+            return getProtectRegionNameByIntersection(selection);
+        } catch (NoSelectionException ignored) {}
+        return null;
+    }
+
+    /**
+     * Get region name.
+     *
+     * @param selection get selection in the region by player.
      *
      * @return location of Object.
      */
-    public String getProtectRegionName(@NotNull Location location) {
+    public String getProtectRegionNameByIntersection(final Region selection) throws NoSelectionException {
+        if (selection instanceof CuboidRegion) {
+            final BlockVector3 min = selection.getMinimumPoint();
+            final BlockVector3 max = selection.getMaximumPoint();
+            final RegionContainer rc = WorldGuard.getInstance().getPlatform().getRegionContainer();
+            final RegionManager regions = rc.get(selection.getWorld());
+            final ProtectedRegion __dummy__ = new ProtectedCuboidRegion("__dummy__", min, max);
+            assert regions != null;
+            ApplicableRegionSet applicableRegionSet = regions.getApplicableRegions(__dummy__);
+            return applicableRegionSet
+                    .getRegions()
+                    .stream()
+                    .map(ProtectedRegion::getId)
+                    .collect(Collectors.joining());
+        }
+        throw new NoSelectionException();
+    }
+
+    /**
+     * Get region name.
+     *
+     * @param location get location of Object in the region.
+     *
+     * @return location of Object.
+     */
+    public String getProtectName(@NotNull Location location) {
         final ApplicableRegionSet applicableRegionSet = this.getApplicableRegions(location);
         return applicableRegionSet
                 .getRegions()
