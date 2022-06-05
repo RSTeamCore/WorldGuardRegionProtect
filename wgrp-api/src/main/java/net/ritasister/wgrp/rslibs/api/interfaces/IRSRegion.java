806a60b9299f0c909843db50f1bb6750ef961eba
RitaWolf <ritasistr@mail.ru>
2022-06-05 17:02:39 +0300

    created and moved one method for get region name

--- wgrp-api/src/main/java/net/ritasister/wgrp/rslibs/api/interfaces/IRSRegion.java
+++ wgrp-api/src/main/java/net/ritasister/wgrp/rslibs/api/interfaces/IRSRegion.java
@@ -1,6 +1,7 @@
 package net.ritasister.wgrp.rslibs.api.interfaces;
 
 import org.bukkit.Location;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
@@ -10,4 +11,5 @@ public interface IRSRegion {
 
     boolean checkStandingRegion(@NotNull Location location);
     boolean checkStandingRegion(@NotNull Location location, HashMap<String, List<String>> regions);
+    String getProtectRegionName(Player player);
 }
