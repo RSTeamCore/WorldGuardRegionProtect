8ad536df6536efd86274682bb359e7196608b0e2
RitaWolf <ritasistr@mail.ru>
2022-06-05 11:33:34 +0300

    fixed one more NPE

--- wgrp-api/src/main/java/net/ritasister/wgrp/rslibs/api/interfaces/IRSRegion.java
+++ wgrp-api/src/main/java/net/ritasister/wgrp/rslibs/api/interfaces/IRSRegion.java
@@ -1,7 +1,6 @@
 package net.ritasister.wgrp.rslibs.api.interfaces;
 
 import org.bukkit.Location;
-import org.bukkit.World;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
@@ -9,6 +8,6 @@ import java.util.List;
 
 public interface IRSRegion {
 
-    boolean checkStandingRegion(@NotNull Location location, HashMap<String, List<String>> regions);
     boolean checkStandingRegion(@NotNull Location location);
+    boolean checkStandingRegion(@NotNull Location location, HashMap<String, List<String>> regions);
 }
