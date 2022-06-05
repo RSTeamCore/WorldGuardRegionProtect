a1334b0f8a4a55b41a3a1a4ad47314be15533c87
RitaWolf <ritasistr@mail.ru>
2022-06-05 17:01:52 +0300

    added 2 new exceptions

---
+++ wgrp-bukkit/src/main/java/net/ritasister/wgrp/rslibs/exceptions/NoSelectionException.java
@@ -0,0 +1,8 @@
+package net.ritasister.wgrp.rslibs.exceptions;
+
+public class NoSelectionException extends Exception {
+
+    public NoSelectionException() {
+        super("Selection not has been made!");
+    }
+}
---
+++ wgrp-bukkit/src/main/java/net/ritasister/wgrp/rslibs/exceptions/UnknownRegionException.java
@@ -0,0 +1,8 @@
+package net.ritasister.wgrp.rslibs.exceptions;
+
+public class UnknownRegionException extends Exception {
+
+    public UnknownRegionException() {
+        super("Region does not exists!");
+    }
+}
