302eebfd3db466ca732218e0745d87b3fd4ab10b
RitaWolf <ritasistr@mail.ru>
2022-06-05 17:00:46 +0300

    fixed error with database connection leaks

--- wgrp-bukkit/src/main/java/net/ritasister/wgrp/rslibs/datasource/Storage.java
+++ wgrp-bukkit/src/main/java/net/ritasister/wgrp/rslibs/datasource/Storage.java
@@ -15,6 +15,7 @@ import java.sql.SQLException;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
+@SuppressWarnings({"checkstyle:AbbreviationAsWordInName"})
 public class Storage implements StorageDataSource {
 
 	private final WorldGuardRegionProtect wgRegionProtect;
@@ -40,8 +41,9 @@ public class Storage implements StorageDataSource {
 		// Pool settings
         config.setMaximumPoolSize(wgRegionProtect.getUtilConfig().getConfig().getMySQLSettings().getMaxPoolSize());
         config.setMaxLifetime(wgRegionProtect.getUtilConfig().getConfig().getMySQLSettings().getMaxLifetime() * 1000L);
-        config.setConnectionTimeout(wgRegionProtect.getUtilConfig().getConfig().getMySQLSettings().getConnectionTimeout());
-				
+		config.addDataSourceProperty("useSSL", wgRegionProtect.getUtilConfig().getConfig().getMySQLSettings().getUseSsl());
+		config.setConnectionTimeout(wgRegionProtect.getUtilConfig().getConfig().getMySQLSettings().getConnectionTimeout());
+
 		config.setPoolName("MariaDBPool");
 		
         // Encoding
@@ -62,11 +64,7 @@ public class Storage implements StorageDataSource {
 	}
 
 	private Connection getConnection() throws SQLException {
-		if (!this.ds.getConnection().isValid(6)) {
-			wgRegionProtect.getRsApi().getLogger().error("Lost connection to database. Retrying connect...");
-			this.connect();
-		}
-		return this.ds.getConnection();
+		return ds.getConnection();
 	}
 
 	private void initialize() {
@@ -83,6 +81,7 @@ public class Storage implements StorageDataSource {
 			this.close(pst);
 		}
 	}
+
 	@Override
 	public boolean load() {
 		PreparedStatement pst = null;
@@ -118,45 +117,46 @@ public class Storage implements StorageDataSource {
 
 	public void loadAsync() {
 		Bukkit.getScheduler().runTaskTimerAsynchronously(wgRegionProtect.getWgrpBukkitPlugin(), () -> {
-					ConcurrentHashMap<UUID, StorageDataBase> tempDataBase = new ConcurrentHashMap<>();
-					PreparedStatement pst = null;
-					ResultSet rs = null;
-					try (Connection conn = Storage.this.getConnection()) {
-						pst = conn.prepareStatement(
-								"SELECT * FROM <table>;"
-										.replace("<table>", wgRegionProtect.getUtilConfig().getConfig().getMySQLSettings().getTable()));
-						rs = pst.executeQuery();
-						while (rs.next()) {
-							UUID uniqueId = UUID.fromString(rs.getString("uniqueId"));
-							StorageDataBase dataBase = new StorageDataBase(
-									rs.getInt("id"),
-									rs.getString("nickname"),
-									uniqueId,
-									rs.getLong("time"),
-									rs.getString("action"),
-									rs.getString("region"),
-									rs.getString("world"),
-									rs.getDouble("x"),
-									rs.getDouble("y"),
-									rs.getDouble("z"));
-							tempDataBase.put(uniqueId, dataBase);
-						}
-						wgRegionProtect.getRsStorage().dbLogs = new ConcurrentHashMap<>(tempDataBase);
-					} catch (SQLException ex) {
-						wgRegionProtect.getRsApi().getLogger().error("Failed to load database asynchronous!");
-						ex.printStackTrace();
-					} finally {
-						Storage.this.close(rs);
-						Storage.this.close(pst);
-					}
-				}, wgRegionProtect.getUtilConfig().getConfig().getMySQLSettings().getIntervalReload() * 20L,
+			ConcurrentHashMap<UUID, StorageDataBase> tempDataBase = new ConcurrentHashMap<>();
+			PreparedStatement pst = null;
+			ResultSet rs = null;
+			try (Connection conn = Storage.this.getConnection()) {
+				pst = conn.prepareStatement(
+						"SELECT * FROM <table>;"
+								.replace("<table>", wgRegionProtect.getUtilConfig().getConfig().getMySQLSettings().getTable()));
+				rs = pst.executeQuery();
+				while (rs.next()) {
+					UUID uniqueId = UUID.fromString(rs.getString("uniqueId"));
+					StorageDataBase dataBase = new StorageDataBase(
+							rs.getInt("id"),
+							rs.getString("nickname"),
+							uniqueId,
+							rs.getLong("time"),
+							rs.getString("action"),
+							rs.getString("region"),
+							rs.getString("world"),
+							rs.getDouble("x"),
+							rs.getDouble("y"),
+							rs.getDouble("z"));
+					tempDataBase.put(uniqueId, dataBase);
+				}
+				wgRegionProtect.getRsStorage().dbLogs = new ConcurrentHashMap<>(tempDataBase);
+			} catch (SQLException ex) {
+				wgRegionProtect.getRsApi().getLogger().error("Failed to load database asynchronous!");
+				ex.printStackTrace();
+			} finally {
+				this.close(rs);
+				this.close(pst);
+			}
+			}, wgRegionProtect.getUtilConfig().getConfig().getMySQLSettings().getIntervalReload() * 20L,
 				wgRegionProtect.getUtilConfig().getConfig().getMySQLSettings().getIntervalReload() * 20L);
 	}
 
 	@Override
 	public void setLogAction(String nickname, @NotNull UUID uniqueId, long time, @NotNull RegionAction action, String region, String world, int x, int y, int z) {
-		PreparedStatement pst = null;
-		try(Connection conn = this.getConnection()) {
+		Bukkit.getScheduler().runTaskAsynchronously(wgRegionProtect.getWgrpBukkitPlugin(), () -> {
+			PreparedStatement pst = null;
+			try(Connection conn = this.getConnection()) {
 				pst = conn.prepareStatement("INSERT INTO <table> (nickname, uniqueId, time, action, region, world, x, y, z) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);"
 						.replace("<table>", wgRegionProtect.getUtilConfig().getConfig().getMySQLSettings().getTable()));
 				pst.setString(1, nickname);
@@ -170,11 +170,12 @@ public class Storage implements StorageDataSource {
 				pst.setDouble(9, z);
 				pst.executeUpdate();
 			} catch (SQLException ex) {
-			wgRegionProtect.getRsApi().getLogger().error("[MySQL] <id> "+uniqueId.toString()
+			wgRegionProtect.getRsApi().getLogger().error("[DataBase] <id> "+uniqueId.toString()
 					.replace("<id>", uniqueId.toString())+ ex);
-		} finally {
-			this.close(pst);
-		}
+			} finally {
+				this.close(pst);
+			}
+		});
 	}
 
 	public void close(final PreparedStatement pst) {
