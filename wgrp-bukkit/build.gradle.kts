a999640b76605e9fa4a194cdb6c9b5704c97bab3
RitaWolf <ritasistr@mail.ru>
2022-12-04 23:54:25 +0200

    added guice and updated version of depend

--- wgrp-bukkit/build.gradle.kts
+++ wgrp-bukkit/build.gradle.kts
@@ -31,17 +31,24 @@ dependencies {
     implementation("org.mariadb.jdbc:mariadb-java-client:3.0.6")
     //Inject
     implementation("com.google.inject:guice:5.1.0")
+    implementation("com.google.inject.extensions:guice-assistedinject:5.1.0")
 
     //Annotations
     compileOnly("org.jetbrains:annotations:23.0.0")
-    compileOnly("org.projectlombok:lombok:1.18.24")
-    compileOnly("net.kyori:adventure-api:4.11.0")
+
+    implementation("org.projectlombok:lombok:1.18.24")
+    annotationProcessor("org.projectlombok:lombok:1.18.24")
+
+    implementation("org.projectlombok:lombok:1.18.24")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.24")
+
+    compileOnly("net.kyori:adventure-api:4.12.0")
     //PlaceholderAPI
     compileOnly("me.clip:placeholderapi:2.11.2")
     //WorldGuard 7+
     compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.7")
     //Paper 1.19
-    compileOnly("io.papermc.paper:paper-api:1.19.2-R0.1-SNAPSHOT")
+    compileOnly(dependencyNotation = "io.papermc.paper:paper-api:1.19.2-R0.1-SNAPSHOT")
 
 }
 
