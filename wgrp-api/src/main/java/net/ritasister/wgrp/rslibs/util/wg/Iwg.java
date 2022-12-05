1646ca14fbb6717b9050fbdd7b7fe2ed5ebcb179
RitaWolf <ritasistr@mail.ru>
2022-12-05 11:56:03 +0200

    removed Assisted annotated

--- wgrp-api/src/main/java/net/ritasister/wgrp/rslibs/util/wg/Iwg.java
+++ wgrp-api/src/main/java/net/ritasister/wgrp/rslibs/util/wg/Iwg.java
@@ -1,14 +1,13 @@
 package net.ritasister.wgrp.rslibs.util.wg;
 
-import com.google.inject.assistedinject.Assisted;
 import com.sk89q.worldedit.IncompleteRegionException;
 import org.bukkit.entity.Player;
 
 public interface Iwg {
-   @Assisted boolean checkIntersection(Player player) throws IncompleteRegionException;
-   @Assisted boolean checkCIntersection(Player player, String... args) throws IncompleteRegionException;
-   @Assisted boolean checkPIntersection(Player player, String... args) throws IncompleteRegionException;
-   @Assisted boolean checkSIntersection(Player player, String... args) throws IncompleteRegionException;
-   @Assisted boolean checkUIntersection(Player player, String... args) throws IncompleteRegionException;
-   @Assisted boolean checkCPIntersection(Player player, String... args) throws IncompleteRegionException;
+   boolean checkIntersection(Player player) throws IncompleteRegionException;
+   boolean checkCIntersection(Player player, String... args) throws IncompleteRegionException;
+   boolean checkPIntersection(Player player, String... args) throws IncompleteRegionException;
+   boolean checkSIntersection(Player player, String... args) throws IncompleteRegionException;
+   boolean checkUIntersection(Player player, String... args) throws IncompleteRegionException;
+   boolean checkCPIntersection(Player player, String... args) throws IncompleteRegionException;
 }
