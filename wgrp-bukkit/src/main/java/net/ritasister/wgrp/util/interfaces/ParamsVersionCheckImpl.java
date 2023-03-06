c15f9e11131bcea018985095d010d12300245f78
RitaWolf <ritasistr@mail.ru>
2023-03-06 15:42:25 +0200

    added params for checking version lang

---
+++ wgrp-bukkit/src/main/java/net/ritasister/wgrp/util/interfaces/ParamsVersionCheckImpl.java
@@ -0,0 +1,29 @@
+package net.ritasister.wgrp.util.interfaces;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.jetbrains.annotations.NotNull;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Objects;
+
+public interface ParamsVersionCheckImpl {
+
+    @NotNull default String getSimpleDateFormat() {
+        Date date = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy.MM.dd-hh.mm.ss");
+        return simpleDateFormat.format(date);
+    }
+
+    @NotNull default String getCurrentVersion(final @NotNull YamlConfiguration currentYaml) {
+        return Objects.requireNonNull(currentYaml.getString("langTitle.version"))
+                .replaceAll("\"", "")
+                .replaceAll("'", "");
+    }
+
+    @NotNull default String getNewVersion(final @NotNull YamlConfiguration yamlConfiguration) {
+        return Objects.requireNonNull(yamlConfiguration.getString("langTitle.version"))
+                .replaceAll("\"", "")
+                .replaceAll("'", "");
+    }
+}
