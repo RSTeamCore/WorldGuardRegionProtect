633cd6af5e50346521d847f7460a8c913d417152
RitaWolf <nik30003@gmail.com>
2023-04-22 02:16:57 +0300

    an extra star has been removed in javadoc

--- wgrp-bukkit/src/main/java/net/ritasister/wgrp/rslibs/exceptions/NoSelectionException.java
+++ wgrp-bukkit/src/main/java/net/ritasister/wgrp/rslibs/exceptions/NoSelectionException.java
@@ -1,6 +1,6 @@
 package net.ritasister.wgrp.rslibs.exceptions;
 
-/***
+/**
  * The exception for catching an error if the region is not selected.
  */
 public class NoSelectionException extends RuntimeException {
