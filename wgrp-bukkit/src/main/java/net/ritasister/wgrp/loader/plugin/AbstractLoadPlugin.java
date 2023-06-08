7592d5d60cc9e391d60a9954e1ed983092458b61
RitaWolf <ritasistr@mail.ru>
2023-06-08 18:30:18 +0300

    removed redundant method

--- wgrp-bukkit/src/main/java/net/ritasister/wgrp/loader/plugin/AbstractLoadPlugin.java
+++ wgrp-bukkit/src/main/java/net/ritasister/wgrp/loader/plugin/AbstractLoadPlugin.java
@@ -3,9 +3,6 @@ package net.ritasister.wgrp.loader.plugin;
 public abstract class AbstractLoadPlugin<V> implements LoadPluginImpl<V> {
 
     @Override
-    public void loadPlugin() {}
-
-    @Override
     public abstract V hookPlugin();
 
 }
