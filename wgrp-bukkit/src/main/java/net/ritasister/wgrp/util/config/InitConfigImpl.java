9887dd2730488eee0e9046abb4a20abea4690a72
RitaWolf <ritasistr@mail.ru>
2023-06-09 01:15:05 +0300

    moved to another package

--- wgrp-bukkit/src/main/java/net/ritasister/wgrp/util/config/InitConfigImpl.java
+++ wgrp-bukkit/src/main/java/net/ritasister/wgrp/util/config/InitConfigImpl.java
@@ -1,4 +1,4 @@
-package net.ritasister.wgrp.util.config.impl;
+package net.ritasister.wgrp.util.config;
 
 import net.ritasister.wgrp.WGRPBukkitPlugin;
 import net.ritasister.wgrp.util.config.interfaces.InitConfig;
--- wgrp-bukkit/src/main/java/net/ritasister/wgrp/util/config/ParamsVersionCheckImpl.java
+++ wgrp-bukkit/src/main/java/net/ritasister/wgrp/util/config/ParamsVersionCheckImpl.java
@@ -1,4 +1,4 @@
-package net.ritasister.wgrp.util.config.impl;
+package net.ritasister.wgrp.util.config;
 
 import net.ritasister.wgrp.util.config.interfaces.ParamsVersionCheck;
 import org.bukkit.configuration.file.YamlConfiguration;
