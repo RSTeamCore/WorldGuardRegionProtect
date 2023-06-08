db1aad65bebaac6510b1007feac2becffc302bd6
RitaWolf <ritasistr@mail.ru>
2023-06-08 18:35:03 +0300

    merged two methods into one.

--- wgrp-bukkit/src/main/java/net/ritasister/wgrp/loader/WGRPLoaderCommandsAndListeners.java
+++ wgrp-bukkit/src/main/java/net/ritasister/wgrp/loader/WGRPLoaderCommandsAndListeners.java
@@ -6,16 +6,13 @@ import net.ritasister.wgrp.handler.CommandHandler;
 import net.ritasister.wgrp.handler.ListenerHandler;
 import org.bukkit.plugin.PluginManager;
 
-public class WGRPLoaderCommandsAndListeners extends LoaderCommandsAndListenersImpl<WorldGuardRegionProtect> {
+public class WGRPLoaderCommandsAndListeners extends LoadHandlersImpl<WorldGuardRegionProtect> {
 
     @Override
-    public void loadCommands(WorldGuardRegionProtect worldGuardRegionProtect) {
+    public void loadHandlers(WorldGuardRegionProtect worldGuardRegionProtect) {
         AbstractHandler<Void> registerCommands = new CommandHandler(worldGuardRegionProtect);
         registerCommands.handle();
-    }
 
-    @Override
-    public void loadListeners(WorldGuardRegionProtect worldGuardRegionProtect) {
         AbstractHandler<PluginManager> registerListeners = new ListenerHandler(worldGuardRegionProtect);
         registerListeners.handle(worldGuardRegionProtect.getWgrpContainer().getPluginManager());
     }
