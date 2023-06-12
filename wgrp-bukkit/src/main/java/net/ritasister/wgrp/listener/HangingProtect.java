f38c41036fee4a2093983e7741f9982da97aec23
RitaWolf <ritasistr@mail.ru>
2023-06-12 14:49:58 +0300

    added possibly disable checks

--- wgrp-bukkit/src/main/java/net/ritasister/wgrp/listener/HangingProtect.java
+++ wgrp-bukkit/src/main/java/net/ritasister/wgrp/listener/HangingProtect.java
@@ -1,6 +1,7 @@
 package net.ritasister.wgrp.listener;
 
 import net.ritasister.wgrp.WGRPContainer;
+import net.ritasister.wgrp.util.config.Config;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -12,17 +13,26 @@ public class HangingProtect implements Listener {
 
     private final WGRPContainer wgrpContainer;
 
+    private final Config config;
+
     public HangingProtect(final @NotNull WGRPContainer wgrpContainer) {
         this.wgrpContainer = wgrpContainer;
-    }
-
-    @EventHandler(priority = EventPriority.LOWEST)
-    private void denyHangingBreakByEntity(@NotNull HangingBreakByEntityEvent e) {
-        wgrpContainer.getRsApi().entityCheck(e, e.getRemover(), e.getEntity());
+        this.config = wgrpContainer.getConfig();
     }
 
     @EventHandler(priority = EventPriority.LOWEST)
     private void denyHangingPlace(@NotNull HangingPlaceEvent e) {
+        if (!config.isDenyPlaceItemFrameOrPainting()) {
+            return;
+        }
         wgrpContainer.getRsApi().entityCheck(e, e.getPlayer(), e.getEntity());
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    private void denyHangingBreakByEntity(@NotNull HangingBreakByEntityEvent e) {
+        if (!config.isDenyDamageItemFrameOrPainting()) {
+            return;
+        }
+        wgrpContainer.getRsApi().entityCheck(e, e.getRemover(), e.getEntity());
     }
 }
