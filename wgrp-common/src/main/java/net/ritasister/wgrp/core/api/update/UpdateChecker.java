706d10b8dd9d4276c2daf4d8b9604030fe22d8a1
Rita <ritasistr@mail.ru>
2024-05-06 12:48:36 +0300

    refactor code #2 for 1.20.5-1.20.6

---
+++ wgrp-common/src/main/java/net/ritasister/wgrp/core/api/update/UpdateChecker.java
@@ -0,0 +1,39 @@
+package net.ritasister.wgrp.core.api.update;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Consumer;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.util.Scanner;
+
+public class UpdateChecker {
+
+    private final JavaPlugin plugin;
+    private final int resourceId;
+
+    public UpdateChecker(JavaPlugin plugin, int resourceId) {
+        this.plugin = plugin;
+        this.resourceId = resourceId;
+    }
+
+    /**
+     * Method checker if plugin have a last version.
+     */
+    public void getVersion(final Consumer<String> consumer) {
+        Bukkit.getScheduler().runTaskAsynchronously(this.plugin, () -> {
+            try (InputStream inputStream = URI.create("https://api.spigotmc.org/legacy/update.php?resource="
+                    + this.resourceId).toURL().openStream();
+                 Scanner scanner = new Scanner(inputStream)) {
+                if (scanner.hasNext()) {
+                    consumer.accept(scanner.next());
+                }
+            } catch (IOException exception) {
+                this.plugin.getLogger().info("Cannot look for updates: " + exception.getMessage());
+            }
+        });
+    }
+
+}
