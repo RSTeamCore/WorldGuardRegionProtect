be4c4de489ad8e956b90380d7d6ff931b8754d0f
Rita <ritasistr@mail.ru>
2024-05-07 13:40:39 +0300

    fix get api #2

--- wgrp-api/src/main/java/net/ritasister/wgrp/api/WorldGuardRegionProtectProvider.java
+++ wgrp-api/src/main/java/net/ritasister/wgrp/api/WorldGuardRegionProtectProvider.java
@@ -1,15 +1,14 @@
 package net.ritasister.wgrp.api;
 
+import org.checkerframework.checker.nullness.qual.NonNull;
 import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.Nullable;
 
 /**
  * Provider to get the WorldGuardRegionProtect api instance.
  */
 public final class WorldGuardRegionProtectProvider {
 
-    private static WorldGuardRegionProtect worldGuardRegionProtect;
-    private static WorldGuardRegionMetadata metadata;
+    private static WorldGuardRegionProtect instance;
 
     @ApiStatus.Internal
     private WorldGuardRegionProtectProvider() {
@@ -18,19 +17,45 @@ public final class WorldGuardRegionProtectProvider {
 
     @ApiStatus.Internal
     public static void setWorldGuardRegionProtect(final WorldGuardRegionProtect worldGuardRegionProtect) {
-        if (WorldGuardRegionProtectProvider.worldGuardRegionProtect != null) {
-            throw new IllegalArgumentException("MaintenanceProvider is already set!");
+        if (WorldGuardRegionProtectProvider.instance != null) {
+            throw new IllegalArgumentException("WorldGuardRegionProtectProvider is already set!");
         }
-        WorldGuardRegionProtectProvider.worldGuardRegionProtect = worldGuardRegionProtect;
+        WorldGuardRegionProtectProvider.instance = worldGuardRegionProtect;
     }
 
     /**
-     * Returns the maintenance api instance, or null if not loaded yet.
+     * Gets an instance of the {@link WorldGuardRegionProtect} API,
+     * throwing {@link IllegalStateException} if the API is not loaded yet.
      *
-     * @return maintenance api instance
+     * <p>This method will never return null.</p>
+     *
+     * @return an instance of the LuckPerms API
+     * @throws IllegalStateException if the API is not loaded yet
      */
-    public static @Nullable WorldGuardRegionProtect get() {
-        return worldGuardRegionProtect;
+    public static @NonNull WorldGuardRegionProtect get() {
+        WorldGuardRegionProtect instance = WorldGuardRegionProtectProvider.instance;
+        if (instance == null) {
+            throw new NotLoadedException();
+        }
+        return instance;
+    }
+
+    /**
+     * Exception thrown when the API is requested before it has been loaded.
+     */
+    private static final class NotLoadedException extends IllegalStateException {
+
+        private static final String MESSAGE = "The WorldGuardRegionProtect API isn't loaded yet!\n" +
+                "This could be because:\n" +
+                "  a) the WorldGuardRegionProtect plugin is not installed or it failed to enable\n" +
+                "  b) the plugin in the stacktrace does not declare a dependency on LuckPerms\n" +
+                "  c) the plugin in the stacktrace is retrieving the API before the plugin 'enable' phase\n" +
+                "     (call the #get method in onEnable, not the constructor!)\n";
+
+        NotLoadedException() {
+            super(MESSAGE);
+        }
+
     }
 
 }
