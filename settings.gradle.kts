953decf8ddbbaa384702512aa7f6573869afbe18
Rita Wolf <ritasistr@mail.ru>
2024-06-19 21:39:44 +0300

    remove brackets

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,7 @@ pluginManagement {
     }
     plugins {
         kotlin("jvm") version "2.0.0"
-        id("org.gradle.toolchains.foojay-resolver-convention") version("0.8.0")
+        id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
     }
 }
 
