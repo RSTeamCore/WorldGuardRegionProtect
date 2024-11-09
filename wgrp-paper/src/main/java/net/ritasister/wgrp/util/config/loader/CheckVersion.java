4ecbdfc76e8ba41713f7daac46378dbd8c2318a6
Rita Wolf <ritasistr@mail.ru>
2024-11-09 14:12:35 +0200

    removed unused imports

--- wgrp-paper/src/main/java/net/ritasister/wgrp/util/config/loader/CheckVersion.java
+++ wgrp-paper/src/main/java/net/ritasister/wgrp/util/config/loader/CheckVersion.java
@@ -1,8 +1,6 @@
 package net.ritasister.wgrp.util.config.loader;
 
 import net.ritasister.wgrp.WorldGuardRegionProtectPaperPlugin;
-import net.ritasister.wgrp.util.config.Config;
-import org.jetbrains.annotations.NotNull;
 
 public interface CheckVersion {
 
