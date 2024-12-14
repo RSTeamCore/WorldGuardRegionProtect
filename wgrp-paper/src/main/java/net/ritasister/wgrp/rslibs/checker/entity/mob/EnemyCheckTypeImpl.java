487b2af989e741aa0f73b2493962f462b5d81b77
Rita Wolf <ritasistr@mail.ru>
2024-12-14 13:47:00 +0200

    migrate some mobs from monster to enemy type

---
+++ wgrp-paper/src/main/java/net/ritasister/wgrp/rslibs/checker/entity/mob/EnemyCheckTypeImpl.java
@@ -0,0 +1,38 @@
+package net.ritasister.wgrp.rslibs.checker.entity.mob;
+
+import net.ritasister.wgrp.WorldGuardRegionProtectPaperPlugin;
+import net.ritasister.wgrp.api.model.entity.EntityCheckType;
+import net.ritasister.wgrp.util.file.config.ConfigFields;
+import org.bukkit.entity.Enemy;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+public final class EnemyCheckTypeImpl implements EntityCheckType<Entity, EntityType> {
+
+    private final WorldGuardRegionProtectPaperPlugin wgrpPlugin;
+
+    public EnemyCheckTypeImpl(final WorldGuardRegionProtectPaperPlugin wgrpPlugin) {
+        this.wgrpPlugin = wgrpPlugin;
+    }
+
+    @Override
+    public boolean check(final Entity entity) {
+        final Enemy enemy = (Enemy) entity;
+        final EntityType monsterType = enemy.getType();
+        return ConfigFields.ENEMY_TYPE.getList(wgrpPlugin.getWgrpPaperBase()).contains(monsterType.name().toLowerCase());
+    }
+
+    @Contract(value = " -> new", pure = true)
+    @Override
+    public EntityType @NotNull [] getEntityType() {
+        return new EntityType[]{
+                EntityType.GHAST,
+                EntityType.SLIME,
+                EntityType.MAGMA_CUBE,
+                EntityType.PHANTOM
+        };
+    }
+
+}
