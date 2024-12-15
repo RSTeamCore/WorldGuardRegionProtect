77bdbb29e00004b270ce2b79ee5da522815a43e8
Rita Wolf <ritasistr@mail.ru>
2024-12-15 21:09:58 +0200

    remove unused import

--- wgrp-paper/src/main/java/net/ritasister/wgrp/rslibs/updater/UpdateChecker.java
+++ wgrp-paper/src/main/java/net/ritasister/wgrp/rslibs/updater/UpdateChecker.java
@@ -8,7 +8,6 @@ import org.bukkit.Bukkit;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
