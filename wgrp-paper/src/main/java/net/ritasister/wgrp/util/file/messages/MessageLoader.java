9e4b9ec0e6213a292ce61fd7f6395c397fceca7d
Rita Wolf <ritasistr@mail.ru>
2025-01-27 18:18:17 +0200

    refactoring

--- wgrp-paper/src/main/java/net/ritasister/wgrp/util/file/messages/MessageLoader.java
+++ wgrp-paper/src/main/java/net/ritasister/wgrp/util/file/messages/MessageLoader.java
@@ -3,14 +3,16 @@ package net.ritasister.wgrp.util.file.messages;
 import net.ritasister.wgrp.WorldGuardRegionProtectPaperPlugin;
 import net.ritasister.wgrp.util.file.config.ConfigFields;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 
-public class MessageLoader {
+public final class MessageLoader {
 
-    public Messages initMessages(@NotNull final WorldGuardRegionProtectPaperPlugin wgrpPlugin) {
-        final String lang = ConfigFields.LANG.get(wgrpPlugin.getWgrpPaperBase()).toString();
+    @Contract("_ -> new")
+    public @NotNull Messages initMessages(@NotNull final WorldGuardRegionProtectPaperPlugin wgrpPlugin) {
+        final String lang = ConfigFields.LANG.get(wgrpPlugin).toString();
         final File file = new File(wgrpPlugin.getWgrpPaperBase().getDataFolder(), "lang/" + lang + ".yml");
         if (!file.exists()) {
             wgrpPlugin.getWgrpPaperBase().saveResource("lang/" + lang + ".yml", false);
