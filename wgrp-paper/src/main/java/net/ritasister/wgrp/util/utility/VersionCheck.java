ce37d817d5639022f2656a8fb021198fd73c043e
Rita Wolf <ritasistr@mail.ru>
2025-06-18 14:51:39 +0300

    bump paper dev bundle to mc 1.21.6

--- wgrp-paper/src/main/java/net/ritasister/wgrp/util/utility/VersionCheck.java
+++ wgrp-paper/src/main/java/net/ritasister/wgrp/util/utility/VersionCheck.java
@@ -13,10 +13,10 @@ public class VersionCheck {
         this.wgrpPlugin = plugin;
     }
 
-    private final static String SUPPORTED_VERSION_RANGE = "1.20 - 1.21.5";
+    private final static String SUPPORTED_VERSION_RANGE = "1.20 - 1.21.6";
     private final static Set<String> SUPPORTED_VERSION = Set.of(
             "1.20", "1.20.1", "1.20.2", "1.20.3", "1.20.4", "1.20.5", "1.20.6",
-            "1.21", "1.21.1", "1.21.2", "1.21.3", "1.21.4", "1.21.5");
+            "1.21", "1.21.1", "1.21.2", "1.21.3", "1.21.4", "1.21.5", "1.21.6");
 
     /**
      * Initializes all used NMS classes, constructor fields, and methods.
