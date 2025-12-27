5321d7acf0aa241743af4059defaf53c3490cc93
RitaSister <ritasistr@mail.ru>
2025-12-28 01:01:48 +0200

    implement checking version of file and rework logic work

--- wgrp-paper/src/main/java/net/ritasister/wgrp/util/file/config/version/ConfigCheckVersion.java
+++ wgrp-paper/src/main/java/net/ritasister/wgrp/util/file/config/version/ConfigCheckVersion.java
@@ -1,8 +1,11 @@
-package net.ritasister.wgrp.util.file.config;
+package net.ritasister.wgrp.util.file.config.version;
 
 import net.ritasister.wgrp.WorldGuardRegionProtectPaperPlugin;
 import net.ritasister.wgrp.api.config.ParamsVersionCheck;
-import net.ritasister.wgrp.util.file.CheckVersion;
+import net.ritasister.wgrp.api.config.VersionChecker;
+import net.ritasister.wgrp.util.file.UpdateFile;
+import net.ritasister.wgrp.util.file.config.ConfigType;
+import net.ritasister.wgrp.util.utility.VersionCheck;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.jetbrains.annotations.NotNull;
 
@@ -11,16 +14,18 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 
-public final class ConfigCheckVersion implements CheckVersion {
+public final class ConfigCheckVersion implements VersionChecker<WorldGuardRegionProtectPaperPlugin> {
 
     private final ParamsVersionCheck<ConfigType, YamlConfiguration> paramsVersionCheck;
+    private final UpdateFile updateFile;
 
-    public ConfigCheckVersion(ParamsVersionCheck<ConfigType, YamlConfiguration> paramsVersionCheck) {
+    public ConfigCheckVersion(ParamsVersionCheck<ConfigType, YamlConfiguration> paramsVersionCheck, UpdateFile updateFile) {
         this.paramsVersionCheck = paramsVersionCheck;
+        this.updateFile = updateFile;
     }
 
     @Override
-    public void checkVersion(final @NotNull WorldGuardRegionProtectPaperPlugin wgrpPlugin) {
+    public void check(final @NotNull WorldGuardRegionProtectPaperPlugin wgrpPlugin) {
         wgrpPlugin.getLogger().info("Started checking the new version of the config file...");
 
         final File currentConfigFile = new File(wgrpPlugin.getWgrpPaperBase().getDataFolder(), "config.yml");
@@ -32,13 +37,13 @@ public final class ConfigCheckVersion implements CheckVersion {
         boolean isMustUpdate = true;
         if(currentConfigFile.exists() && paramsVersionCheck.checkMatches(paramsVersionCheck.getCurrentVersion(ConfigType.CONFIG, currentConfigVersion))) {
             wgrpPlugin.getLogger().warn("Field of version in config is invalid. We recreate a new one config replacing corrupted config.");
-            wgrpPlugin.getConfigLoader().getUpdateFile().updateFile(wgrpPlugin, currentConfigFile, ConfigType.CONFIG, null);
+            updateFile.updateConfig(wgrpPlugin, currentConfigFile);
             isMustUpdate = false;
         }
         if(isMustUpdate && currentConfigFile.exists()
                 && !paramsVersionCheck.getCurrentVersion(ConfigType.CONFIG, currentConfigVersion)
                 .equals(paramsVersionCheck.getNewVersion(ConfigType.CONFIG, newVersion))) {
-            wgrpPlugin.getConfigLoader().getUpdateFile().updateFile(wgrpPlugin, currentConfigFile, ConfigType.CONFIG, null);
+            updateFile.updateConfig(wgrpPlugin, currentConfigFile);
             wgrpPlugin.getLogger().info("Found new version of config file, updating this now...");
         } else {
             wgrpPlugin.getLogger().info("No update is required for the config file.");
--- wgrp-paper/src/main/java/net/ritasister/wgrp/util/file/config/version/MessageCheckVersion.java
+++ wgrp-paper/src/main/java/net/ritasister/wgrp/util/file/config/version/MessageCheckVersion.java
@@ -1,10 +1,11 @@
-package net.ritasister.wgrp.util.file.messages;
+package net.ritasister.wgrp.util.file.config.version;
 
 import net.ritasister.wgrp.WorldGuardRegionProtectPaperPlugin;
 import net.ritasister.wgrp.api.config.ParamsVersionCheck;
+import net.ritasister.wgrp.api.config.VersionChecker;
+import net.ritasister.wgrp.util.file.UpdateFile;
 import net.ritasister.wgrp.util.file.config.ConfigType;
-import net.ritasister.wgrp.util.file.CheckVersion;
-import net.ritasister.wgrp.util.file.config.ConfigFields;
+import net.ritasister.wgrp.util.file.config.field.ConfigFields;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.jetbrains.annotations.NotNull;
 
@@ -13,19 +14,21 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 
-public final class MessageCheckVersion implements CheckVersion {
+public final class MessageCheckVersion implements VersionChecker<WorldGuardRegionProtectPaperPlugin> {
 
     private final ParamsVersionCheck<ConfigType, YamlConfiguration> paramsVersionCheck;
+    private final UpdateFile updateFile;
 
-    public MessageCheckVersion(ParamsVersionCheck<ConfigType, YamlConfiguration> paramsVersionCheck) {
+    public MessageCheckVersion(ParamsVersionCheck<ConfigType, YamlConfiguration> paramsVersionCheck, UpdateFile updateFile) {
         this.paramsVersionCheck = paramsVersionCheck;
+        this.updateFile = updateFile;
     }
 
     @Override
-    public void checkVersion(final @NotNull WorldGuardRegionProtectPaperPlugin wgrpPlugin) {
+    public void check(final @NotNull WorldGuardRegionProtectPaperPlugin wgrpPlugin) {
         wgrpPlugin.getLogger().info("Started checking the new version of the language file...");
 
-        final String lang = ConfigFields.LANG.get(wgrpPlugin).toString();
+        final String lang = ConfigFields.LANG.asString(wgrpPlugin.getWgrpPaperBase());
 
         final File currentLangFile = new File(wgrpPlugin.getWgrpPaperBase().getDataFolder(), "lang/" + lang + ".yml");
         final InputStreamReader reader = new InputStreamReader(Objects.requireNonNull(wgrpPlugin.getWgrpPaperBase().getResource("lang/" + lang + ".yml")), StandardCharsets.UTF_8);
@@ -36,14 +39,14 @@ public final class MessageCheckVersion implements CheckVersion {
         boolean isMustUpdate = true;
         if(currentLangFile.exists() && paramsVersionCheck.checkMatches(paramsVersionCheck.getCurrentVersion(ConfigType.LANG, currentLangVersion))) {
             wgrpPlugin.getLogger().warn("Field of version in language file is invalid. We recreate a new one language replacing corrupted config.");
-            wgrpPlugin.getConfigLoader().getUpdateFile().updateFile(wgrpPlugin, currentLangFile, ConfigType.LANG, lang);
+            updateFile.updateLang(wgrpPlugin, currentLangFile, lang);
             isMustUpdate = false;
         }
         if (isMustUpdate && currentLangFile.exists()
                 && !paramsVersionCheck.checkMatches(paramsVersionCheck.getCurrentVersion(ConfigType.LANG, currentLangVersion))
                 && !paramsVersionCheck.getCurrentVersion(ConfigType.LANG, currentLangVersion)
                 .equals(paramsVersionCheck.getNewVersion(ConfigType.LANG, newLangVersion))) {
-            wgrpPlugin.getConfigLoader().getUpdateFile().updateFile(wgrpPlugin, currentLangFile, ConfigType.LANG, lang);
+            updateFile.updateLang(wgrpPlugin, currentLangFile, lang);
             wgrpPlugin.getLogger().info("Found new version of lang file, updating this now...");
         } else {
             wgrpPlugin.getLogger().info("No update is required for the lang file.");
