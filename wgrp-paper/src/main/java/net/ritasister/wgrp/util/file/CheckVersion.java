bfa0eb7108549503f31d44256b3e3edf49964701
RitaSister <ritasistr@mail.ru>
2025-12-28 00:43:25 +0200

    refactoring config of logic

--- wgrp-paper/src/main/java/net/ritasister/wgrp/util/file/CheckVersion.java
+++
@@ -1,9 +0,0 @@
-package net.ritasister.wgrp.util.file;
-
-import net.ritasister.wgrp.WorldGuardRegionProtectPaperPlugin;
-
-public interface CheckVersion {
-
-    void checkVersion(final WorldGuardRegionProtectPaperPlugin wgrpPlugin);
-
-}
