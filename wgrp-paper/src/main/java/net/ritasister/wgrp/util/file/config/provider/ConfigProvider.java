efd055e8d9178c356a15fedc7035ab6c1af10ee4
RitaSister <ritasistr@mail.ru>
2025-12-28 00:44:15 +0200

    create provider for messages and config

---
+++ wgrp-paper/src/main/java/net/ritasister/wgrp/util/file/config/provider/ConfigProvider.java
@@ -0,0 +1,29 @@
+package net.ritasister.wgrp.util.file.config.provider;
+
+import net.ritasister.wgrp.WorldGuardRegionProtectPaperPlugin;
+import net.ritasister.wgrp.util.file.config.files.Config;
+import org.jspecify.annotations.NonNull;
+
+public class ConfigProvider implements net.ritasister.wgrp.api.config.ConfigProvider<WorldGuardRegionProtectPaperPlugin, Config> {
+
+    private Config config;
+
+    @Override
+    public void init(@NonNull WorldGuardRegionProtectPaperPlugin plugin) {
+        this.config = new Config(plugin.getWgrpPaperBase());
+    }
+
+    @Override
+    public Config getConfig() {
+        if (config == null) {
+            throw new IllegalStateException("config provider is not initialized yet! Call init(plugin) first.");
+        }
+        return config;
+    }
+
+    public void saveConfig(String path, Object value) {
+        if (config != null) {
+            config.saveConfig(path, value);
+        }
+    }
+}
