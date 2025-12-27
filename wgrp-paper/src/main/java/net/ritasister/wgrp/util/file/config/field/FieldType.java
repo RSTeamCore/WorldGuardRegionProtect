050f74de4b3c41ceead939c2f6f598d9247c13ef
RitaSister <ritasistr@mail.ru>
2025-12-28 00:42:03 +0200

    refactoring config fields and create fieldtype for correct detect type of field

---
+++ wgrp-paper/src/main/java/net/ritasister/wgrp/util/file/config/field/FieldType.java
@@ -0,0 +1,9 @@
+package net.ritasister.wgrp.util.file.config.field;
+
+/**
+ * Enum representing different types of fields that can be used in configuration files.
+ */
+public enum FieldType {
+    STRING, BOOLEAN, INTEGER, DOUBLE, LONG, FLOAT,
+    STRING_LIST, INTEGER_LIST, DOUBLE_LIST, LONG_LIST, FLOAT_LIST, BOOLEAN_LIST,
+}
